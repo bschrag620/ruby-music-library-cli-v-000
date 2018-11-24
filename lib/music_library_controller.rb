05abff4f68d60f5db5fadcb81369e12a25f79b1d
Brad Schrag <brad.schrag@gmail.com>
2018-11-24 17:28:50 +0000

    automatically backed up by learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,79 @@
+require "pry"
+
+class MusicLibraryController
+  attr_accessor :path
+
+  def initialize(path='./db/mp3s')
+    @path = path
+    music_importer = MusicImporter.new(@path)
+    music_importer.import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    input = true
+    while input != "exit"
+      input = gets.strip
+    end
+  end
+
+  def list_songs
+    i = 1
+    song_list = []
+    Song.all.each do |song|
+      song_list << song.name
+    end
+    song_list.sort.each do |song|
+      current_song = Song.find_by_name(song)
+      puts "#{i}. #{current_song.artist.name} - #{current_song.name} - #{current_song.genre.name}"
+      i += 1
+    end
+  end
+
+  def list_artists
+    i = 1
+    artist_list = []
+    Artist.all.each do |artist|
+      artist_list << artist.name
+    end
+    artist_list.sort.each do |artist|
+      puts "#{i}. #{artist}"
+      i += 1
+    end
+  end
+
+  def list_genres
+    i = 1
+    genre_list = []
+    Genre.all.each do |genre|
+      genre_list << genre.name
+    end
+    genre_list.sort.each do |genre|
+      puts "#{i}. #{genre}"
+      i += 1
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    #artist_name = gets.strip
+    #if !Artist.find_by_name(artist_name)
+      
+    #  self.list_songs_by_artist
+    #else
+    #i = 1
+    #  artist.songs.sort.each do |song|
+    #    puts "#{i}. #{song.name}"
+    #    i += 1
+    #  end
+    #end
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,64 @@
+require_relative "concerns/findable.rb"
+
+class Song
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_accessor :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    if artist
+      self.artist = artist
+    end
+    if genre
+      self.genre = genre
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def self.create(name)
+    new_song = Song.new(name)
+    new_song.save
+    new_song
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(' - ')
+    artist = parts[0]
+    title = parts[1]
+    genre = parts[2].chomp('.mp3')
+    song = self.find_or_create_by_name(title)
+    song.artist = Artist.find_or_create_by_name(artist)
+    song.genre = Genre.find_or_create_by_name(genre)
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
+  end
+
+end
